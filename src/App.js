e3a5bfe65142d691d6ab85fbeba91424ed62667f
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-04-25 17:58:01 +0200

    add image and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Images/AbstractComposition.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+        Get your creative juices flowing
         </p>
       </header>
     </div>
