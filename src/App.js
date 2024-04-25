bb66beaf90eb5bb355977780fe0026f2caef4bfc
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-04-25 18:14:40 +0200

    add material UI, container, grid and typography

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import logo from './Images/AbstractComposition.png';
-import './App.css';
+import { Button } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import logo from "./Images/AbstractComposition.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Get your creative juices flowing
-        </p>
-      </header>
-    </div>
+    <Container className="App">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        className="App-header"
+      >
+        <Grid item>
+          <img src={logo} className="App-logo" alt="Abstract Composition" />
+          <Typography variant="subtitle1" fontSize={10} sx={{ mt: 1, mb: 1 }}>
+            Illustration by Icons 8 from Ouch!
+          </Typography>
+        </Grid>
+        <Typography sx={{ mt: 3, mb: 3 }} variant="h5">
+          Get your creative juices flowing
+        </Typography>
+      </Grid>
+    </Container>
   );
 }
 
